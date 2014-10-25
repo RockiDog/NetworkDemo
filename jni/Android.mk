e77efa64f88a861a875c86c99fd77298d9cee9d1
Wang Ru <rockidog@live.com>
2014-10-25 16:41:34 +0800

    Update native detector

--- jni/Android.mk
+++ jni/Android.mk
@@ -1,9 +1,13 @@
 LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
-include  $(OPENCV4ANDROID_HOME)/native/jni/OpenCV.mk
-LOCAL_MODULE    := CvDetector
-LOCAL_SRC_FILES := com_rockidog_networkdemo_utils_Detector.h
+#OPENCV_CAMERA_MODULES:=off
+#OPENCV_INSTALL_MODULES:=on
+#OPENCV_LIB_TYPE:=SHARED
+include /usr/local/adt/OpenCV/sdk/native/jni/OpenCV.mk
+LOCAL_SRC_FILES := com_rockidog_networkdemo_utils_Detector.cpp
 LOCAL_C_INCLUDES += $(LOCAL_PATH)
+LOCAL_LDLIBS     += -ldl
+LOCAL_MODULE    := CvDetector
 
 include $(BUILD_SHARED_LIBRARY)
--- src/com/rockidog/networkdemo/utils/Detector.java
+++ src/com/rockidog/networkdemo/utils/Detector.java
@@ -1,59 +1,49 @@
 package com.rockidog.networkdemo.utils;
 
-import org.opencv.android.BaseLoaderCallback;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewListener2;
 import org.opencv.android.CameraBridgeViewBase.CvCameraViewFrame;
-import org.opencv.android.LoaderCallbackInterface;
-import org.opencv.android.OpenCVLoader;
 import org.opencv.core.Core;
 import org.opencv.core.Mat;
 import org.opencv.core.MatOfRect;
 import org.opencv.core.Rect;
 import org.opencv.core.Scalar;
 
-import android.content.Context;
-
 public class Detector implements CvCameraViewListener2 {
-    private BaseLoaderCallback mCallback;
-    private long mCascadeClassifier;
+    private static final String TAG = "Detector";
     private Scalar mRectColor = new Scalar(255, 0, 0, 255);
     private int mRectThickness = 3;
 
-    public Detector(Context appContext, String cascadePath) {
-        mCallback = new BaseLoaderCallback(appContext) {
-            @Override
-            public void onManagerConnected(int status) {
-                switch (status) {
-                    case LoaderCallbackInterface.SUCCESS:
-                        System.loadLibrary("CCvDetector");
-                        break;
-                    default:
-                        super.onManagerConnected(status);
-                        break;
-                }
-            }
-        };
-        OpenCVLoader.initAsync(OpenCVLoader.OPENCV_VERSION_2_4_3, appContext, mCallback);
-        mCascadeClassifier = nativeCreate(cascadePath);
+    private long mCascadeClassifier;
+    private Mat mRgbaFrame;
+    private Mat mGrayFrame;
+
+    public Detector(String cascadePath) { mCascadeClassifier = nativeCreate(cascadePath); }
+    public void Destroy() { nativeDestroy(mCascadeClassifier); }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mRgbaFrame = new Mat();
+        mGrayFrame = new Mat();
+        nativeStart(mCascadeClassifier);
     }
 
     @Override
-    public void onCameraViewStarted(int width, int height) {}
-
-    @Override
-    public void onCameraViewStopped() {}
+    public void onCameraViewStopped() {
+        nativeStop(mCascadeClassifier);
+        mRgbaFrame.release();
+        mGrayFrame.release();
+    }
 
     @Override
     public Mat onCameraFrame(CvCameraViewFrame inputFrame) {
-        Mat rgbaFrame = inputFrame.rgba();
-        Mat grayFrame = inputFrame.gray();
+        mRgbaFrame = inputFrame.rgba();
+        mGrayFrame = inputFrame.gray();
         MatOfRect faces = new MatOfRect();
-        nativeDetect(mCascadeClassifier, grayFrame.getNativeObjAddr(), faces.getNativeObjAddr());
+        nativeDetect(mCascadeClassifier, mGrayFrame.getNativeObjAddr(), faces.getNativeObjAddr());
         Rect[] rectArrary = faces.toArray();
-        for (int i = 0; rectArrary.length != i; ++i) {
-            Core.rectangle(rgbaFrame, rectArrary[i].tl(), rectArrary[i].br(), mRectColor, mRectThickness);
-        }
-        return rgbaFrame;
+        for (int i = 0; rectArrary.length != i; ++i)
+            Core.rectangle(mRgbaFrame, rectArrary[i].tl(), rectArrary[i].br(), mRectColor, mRectThickness);
+        return mRgbaFrame;
     }
 
     private native long nativeCreate(String cascadePath);
